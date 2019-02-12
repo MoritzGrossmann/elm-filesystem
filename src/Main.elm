3194de4b9670eaf7bd11837c7b606af8a95b1a89
moritz.grossmann95@googlemail.com <Troopers95?>
2019-02-12 21:19:34 +0100

    initial

---
+++ src/Main.elm
@@ -0,0 +1,41 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html)
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init flags =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    Html.text ""
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
